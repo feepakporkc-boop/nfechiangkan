9bb682c25f83998202478815e150270196f12864
feepakporkc-boop <feepakporkc@gmail.com>
2026-09-02 11:30:42 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbxba2Enfzaz4wu7supLpkW2V-3aQLk4AG6KtPjc6GczcifG4J7wXpcE85pmAZsoKXdBhg/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbzwAk3Q_KUOFLBd1vDhFnt8fqn9R71zwlzE14651zFflCxhUHnStjHbdv0AOghfrzhW/exec'
   });
 })();
