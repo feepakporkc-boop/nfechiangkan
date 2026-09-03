55da0a440628ba45c2d501f29a1f4865f1611afd
feepakporkc-boop <feepakporkc@gmail.com>
2026-09-03 14:29:28 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbyaqla8XlBjCrMg9gZkm6BvWLHN4e_6SJ8OlfsRrWCCN4gwnTodJ0TFq439CPhdvFLa/exec';
+  'https://script.google.com/macros/s/AKfycbzJRnTTAFSVpU9DhYSCtRTLem0W5Tw4nO-c6fkHrtpmq2UX5pAlJzfEsOlG-n3zmthy/exec';
